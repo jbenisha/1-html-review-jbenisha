9cc79642177d34e481f5b274dfd4ef053db2afa6
jbenisha <jbenishay@comcast.net>
2021-10-19 17:55:54 -0400

    hw6

--- app/public/js/books.js
+++ app/public/js/books.js
@@ -1,7 +1,8 @@
 const BookList = {
     data() {
       return {
-        books: []
+        books: [],
+        bookForm:{}
         }
     },
     methods:{
@@ -16,6 +17,28 @@ const BookList = {
                 console.error(err);
             })
         },
+        postNewBook(evt) {
+                
+            
+            console.log("Posting!", this.bookForm);
+    
+            fetch('api/books/create.php', {
+                method:'POST',
+                body: JSON.stringify(this.bookForm),
+                headers: {
+                  "Content-Type": "application/json; charset=utf-8"
+                }
+              })
+              .then( response => response.json() )
+              .then( json => {
+                console.log("Returned from post:", json);
+                // TODO: test a result was returned!
+                this.books = json;
+                
+                // reset the form
+                this.bookForm = {};
+              });
+          }
     },
     created() {
     
